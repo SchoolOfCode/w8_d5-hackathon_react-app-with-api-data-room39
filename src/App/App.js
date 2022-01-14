d3bff6f580bd609a091c95e546b7572c9e869bb6
NovaGlow <dilyncorbett@gmail.com>
2022-01-14 11:22:53 +0000

    pulling data from API

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from '../Header/index.js'
 import Home from '../Home/index.js'
+import Main from '../Main/index.js'
 
 function App() {
   return (
     <div>
+    
       <Header />
       <Home />
+      <Main />
     </div>
   )
 }
--- src/Main/index.js
+++ src/Main/index.js
@@ -5,12 +5,37 @@ import Form from './Form.js'
 import Question from './Question.js'
 
 export default function Main() {
+  const [state, setState] = useState('')
+    useEffect(() => {
+      async function fetchMyAPI() {
+        const response = await fetch(`http://jservice.io/api/random`)
+        const data = await response.json()
+        setState(data)
+          console.log(data)
+        
+      }
+  
+      fetchMyAPI()
+    },[])
   return (
     <div>
-      <Title />
+      {/* <Title />
       <Question />
       <Clue />
-      <Form />
+      <Form /> */}
     </div>
   )
 }
+
+
+
+
+
+
+
+
+  
+  
+
+
+
